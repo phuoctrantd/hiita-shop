4447862adad489d20e77633aaf4bd2c29a31fed6
PhuocTran <phuoctrantd@gmail.com>
2023-12-10 19:38:46 +0700

    fix feedback

--- components/Home/Title.tsx
+++ components/Home/Title.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Stack, Divider, Box, styled, Typography } from "@mui/material";
+import {
+  Stack,
+  Divider,
+  Box,
+  styled,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { red, white } from "@/styles";
 import Link from "next/link";
 interface TitleProps {
   title: string;
+  link?: string;
 }
 
-const Title: React.FC<TitleProps> = ({ title }) => {
+const Title: React.FC<TitleProps> = ({ title, link }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <>
       <Box mb={2.5}>
-        <Stack direction="row" spacing={14}>
+        <Stack
+          direction="row"
+          spacing={14}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
           <Box
             sx={{
               p: 1.6,
@@ -24,6 +40,11 @@ const Title: React.FC<TitleProps> = ({ title }) => {
           >
             {title}
           </Box>
+          {link && !isMobile && (
+            <Link href={link}>
+              <TypographyLink> Xem thêm</TypographyLink>
+            </Link>
+          )}
         </Stack>
         <Divider sx={{ backgroundColor: red[100], height: 3 }} />
       </Box>
