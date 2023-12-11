3589996f32da03983213a1a6ed89a5f2540fae86
PhuocTran <phuoctrantd@gmail.com>
2023-12-11 19:06:49 +0700

    about

---
+++ pages/about/About.tsx
@@ -0,0 +1,14 @@
+import Page from "@/components/Page";
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <Page title="Giới thiệu">
+        <></>
+      </Page>
+    </>
+  );
+};
+
+export default About;
