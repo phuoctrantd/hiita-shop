1334a244fea94f642b944658ef8d1ce563343536
PhuocTran <phuoctrantd@gmail.com>
2023-12-23 20:11:17 +0700

    add register

--- components/Dialog/Register.tsx
+++ components/Dialog/Register.tsx
@@ -6,6 +6,8 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { black, gray, red, white } from "@/styles";
+import { register } from "@/lib/routes/auth";
+import toast from "react-hot-toast";
 interface RegisterProps {
   open: boolean;
   close: () => void;
@@ -17,6 +19,14 @@ const Register: React.FC<RegisterProps> = ({
   handleOpenLogin,
 }) => {
   const validation = z.object({
+    user_name: z
+      .string()
+      .min(1, "Tên đăng nhập là bắt buộc")
+      .max(20, "Tên đăng nhập không được quá 20 ký tự"),
+    phone_number: z
+      .string()
+      .min(1, "Số điện thoại là bắt buộc")
+      .max(20, "Số điện thoại không được quá 20 ký tự"),
     email: z
       .string()
       .min(1, "Email là bắt buộc")
@@ -24,8 +34,8 @@ const Register: React.FC<RegisterProps> = ({
       .email({ message: "Email không hợp lệ" }),
     name: z
       .string()
-      .min(1, "Tên là bắt buộc")
-      .max(30, "Tên không được quá 30 ký tự"),
+      .min(1, "Họ và tên là bắt buộc")
+      .max(30, "Họ và tên không được quá 30 ký tự"),
     password: z
       .string()
       .min(1, "Mật khẩu là bắt buộc")
@@ -42,14 +52,20 @@ const Register: React.FC<RegisterProps> = ({
       email: "",
       name: "",
       password: "",
+      user_name: "",
+      phone_number: "",
     },
     resolver: zodResolver(validation),
   });
   const onSubmit: SubmitHandler<RegisterForm> = async (value) => {
     try {
-      console.log(value);
+      await register(value);
+      toast.success("Đăng ký thành công");
+      handleClose();
+      handleOpenLogin();
       reset();
     } catch (e) {
+      toast.error("Đăng ký thất bại,liên hệ admin để được hỗ trợ");
       return;
     }
   };
@@ -77,14 +93,24 @@ const Register: React.FC<RegisterProps> = ({
           spacing={2}
           sx={{ marginBottom: "20px !important" }}
         >
-          <TextField name="email" placeholder="Email" control={control} />
-          <TextField name="name" placeholder="Họ tên" control={control} />
+          <TextField
+            name="user_name"
+            placeholder="Tên đăng nhập"
+            control={control}
+          />
           <TextField
             name="password"
             placeholder="Mật khẩu"
             control={control}
             type="password"
           />
+          <TextField
+            name="phone_number"
+            placeholder="Số điện thoại"
+            control={control}
+          />
+          <TextField name="email" placeholder="Email" control={control} />
+          <TextField name="name" placeholder="Họ tên" control={control} />
         </Stack>
 
         <Button
--- lib/routes/auth.ts
+++ lib/routes/auth.ts
@@ -3,7 +3,15 @@ interface LoginTypeProps {
     user_name: string;
     password: string;
 }
+interface RegisterTypeProps {
+    user_name: string;
+    password: string;
+    email: string;
+    phone_number: string;
+    name: string;
+}
 const login = (body: LoginTypeProps) => request.post("/login", body)
+const register = (body: RegisterTypeProps) => request.post("/register", body)
 
-export { login }
+export { login,register }
 
