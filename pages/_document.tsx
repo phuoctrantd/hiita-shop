7901b99002a46bca2bd2baf0e994d4a38e960c41
PhuocTran <phuoctrantd@gmail.com>
2024-01-18 21:55:08 +0700

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,18 +4,42 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+               window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml           : true,
+                  version         : 'v18.0'
+                });
+               };
+
+               (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+               })(document, 'script', 'facebook-jssdk');
+             `,
+            }}
+          />
+        </Head>
         <body>
+          <div id="fb-root"></div>
+          <div id="fb-customer-chat" className="fb-customerchat"></div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+               var chatbox = document.getElementById('fb-customer-chat');
+               chatbox.setAttribute("page_id", "159577080578765");
+               chatbox.setAttribute("attribution", "biz_inbox");
+             `,
+            }}
+          />
           <Main />
           <NextScript />
-          <div id="fb-root"></div>
-          <script
-            async
-            defer
-            crossOrigin="anonymous"
-            src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v18.0&appId=412622786898356"
-            nonce="Hwvz64vj"
-          ></script>
         </body>
       </Html>
     );
