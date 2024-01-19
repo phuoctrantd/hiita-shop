abd307763ea3c56943df166268670ad13c01c626
PhuocTran <phuoctv@solashi.com>
2024-01-19 13:21:38 +0700

    test message

--- components/FacebookChatPlugin.tsx
+++ components/FacebookChatPlugin.tsx
@@ -4,21 +4,24 @@ const FacebookChatPlugin = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.innerHTML = `
-     window.fbAsyncInit = function() {
-       FB.init({
-         xfbml          : true,
-         version        : 'v18.0'
-       });
-     };
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml        : true,
+        version      : 'v18.0'
+      });
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "your_page_id");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    };
 
-     (function(d, s, id) {
-       var js, fjs = d.getElementsByTagName(s)[0];
-       if (d.getElementById(id)) return;
-       js = d.createElement(s); js.id = id;
-       js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-       fjs.parentNode.insertBefore(js, fjs);
-     })(document, 'script', 'facebook-jssdk');
-   `;
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  `;
     document.body.appendChild(script);
 
     return () => {
